dabbdb738f157c3d73ce8f6d07d56dc9b0c7b396
NemiMoser <135074383+NemiMoser@users.noreply.github.com>
2024-01-25 19:43:12 -0500

    edited connection

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
-const Sequelize = require('sequelize');
+const { Sequelize } = require('sequelize');
 
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
+    sequelize = new Sequelize(process.env.JAWSDB_URL, 
+    {
+        dialect: 'mysql',
+    });
 } else {
     sequelize = new Sequelize(
         process.env.DB_NAME,
